79c50cba844177dbde5d37c80eca6275f0ff24e9
Bruno <128260096+brndev2@users.noreply.github.com>
2026-05-01 00:24:52 -0300

    Add files via upload

---
+++ 2353060.lua
@@ -0,0 +1,16 @@
+-- invencivel vs
+addappid(2353060) -- jogo
+addappid(2353061, 1, "c3961a7a4786839c1bda96e5698609ac8677615ff115704a380f9a565e9f1204") -- Depot 2353061
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+
+
+-- dlcs
+addappid(4229690) -- Invincible VS - Zero Suit Mark Skin
+addappid(4229700) -- Invincible VS - Deluxe Edition Profile Cosmetics
+addappid(4439910) -- Digital Deluxe Upgrade
+addappid(4439980) -- Conquest Breakout
+addappid(4439990) -- Mark Movincihawk Atom Eve 646
+addappid(4440070) -- Ella Mental Diamond
+addappid(4440100) -- Allen COP
+addappid(4440110) -- Battle Beast Invincible Universe
