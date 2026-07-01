7933b21213cc4d270ff9447e8729b034d2478808
Bruno <128260096+brndev2@users.noreply.github.com>
2026-06-30 22:15:09 -0300

    Add files via upload

---
+++ 1293830.lua
@@ -0,0 +1,73 @@
+
+addappid(1293830) -- Forza Horizon 4
+addappid(1293831, 1, "c6be4a0719d73ba5201e778b7e44d6c9c0e1b4baf33104df4d0da1729751cf7f") -- Sunrise Content
+-- setManifestid(1293831, "7960876371477493357", 88153822411)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+-- setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+-- setManifestid(228990, "1829726630299308803", 102931551)
+addappid(1443091)
+addappid(1443091, 1, "cacce7495ef657d5a7e8ae0f89e83fcdbb3f7146e25f8dcf9a4843a74e711aba") -- Forza Horizon 4 Fortune Island - DLC 1443091 Forza Horizon 4: Fortune Island (1443091) Depot
+-- setManifestid(1443091, "1638530373648163875", 7706213694)
+addappid(1443092)
+addappid(1443092, 1, "bff2e67bf6bdf9001760367fcb99b7d1fba41f17441c648568895843464e2dd3") -- Forza Horizon 4 LEGO Speed Champions - DLC 1443092 Forza Horizon 4: LEGO® Speed Champions (1443092) Depot
+-- setManifestid(1443092, "2636644777724144367", 5689241757)
+addappid(1443010) -- Forza Horizon 4 1979 Talbot Sunbeam Lotus
+addappid(1443011) -- Forza Horizon 4 1967 Sunbeam Tiger
+addappid(1443015) -- Forza Horizon 4 1938 MG TA Midget
+addappid(1443016) -- Forza Horizon 4 2017 Koenigsegg Agera RS
+addappid(1443017) -- Forza Horizon 4 2016 Honda Civic Coupe GRC
+addappid(1443018) -- Forza Horizon 4 2018 Ford Deberti Design Mustang Fastback
+addappid(1443019) -- Forza Horizon 4 2019 Porsche 911 Carrera S
+addappid(1443020) -- Forza Horizon 4 1993 Porsche 968 Turbo S
+addappid(1443021) -- Forza Horizon 4 2005 Honda NSX-R GT
+addappid(1443022) -- Forza Horizon 4 1966 Hillman Imp
+addappid(1443023) -- Forza Horizon 4 1968 Ford Mustang GT 22 Fastback
+addappid(1443024) -- Forza Horizon 4 Ford Transit
+addappid(1443025) -- Forza Horizon 4 1959 Cadillac Eldorado Biarritz Convertible
+addappid(1443026) -- Forza Horizon 4 2018 TVR Griffith
+addappid(1443027) -- Forza Horizon 4 2019 BMW i8 Roadster
+addappid(1443028) -- Forza Horizon 4 2002 Mazda RX-7 Spirit R Type-A
+addappid(1443029) -- Forza Horizon 4 1962 Triumph TR3B
+addappid(1443040) -- Forza Horizon 4 2010 Vauxhall Insignia VXR
+addappid(1443041) -- Forza Horizon 4 2014 McLaren 650 Super Sport Spyder
+addappid(1443042) -- Forza Horizon 4 1953 Jaguar C-Type
+addappid(1443043) -- Forza Horizon 4 1974 Honda CivicRS
+addappid(1443044) -- Forza Horizon 4 2018 Chevrolet Deberti Design DriftTruck
+addappid(1443045) -- Forza Horizon 4 2018 Aston Martin Vantage
+addappid(1443046) -- Forza Horizon 4 1965 Peel Trident
+addappid(1443047) -- Forza Horizon 4 2018 Chevrolet Camaro ZL1 1LE
+addappid(1443048) -- Forza Horizon 4 2019 Chevrolet Corvette ZR1
+addappid(1443049) -- Forza Horizon 4 2018 Morgan Aero GT
+addappid(1443050) -- Forza Horizon 4 1970 Triumph TR6 PI
+addappid(1443051) -- Forza Horizon 4 2017 Ferrari GTC4Lusso
+addappid(1443052) -- Forza Horizon 4 1972 Lamborghini Jarama S
+addappid(1443053) -- Forza Horizon 4 1966 Volkswagen Double Cab Pick-Up
+addappid(1443054) -- Forza Horizon 4 2018 Can-Am Maverick X3 X RS Turbo R
+addappid(1443055) -- Forza Horizon 4 1929 Mercedes-Benz SSK
+addappid(1443056) -- Forza Horizon 4 1985 Porsche 186 959 Paris-Dakar
+addappid(1443057) -- Forza Horizon 4 2018 Nissan SentraNismo
+addappid(1443058) -- Forza Horizon 4 2018 Alfa Romeo Stelvio Quadrifoglio
+addappid(1443059) -- Forza Horizon 4 2005 Ferrari FXX
+addappid(1443060) -- Forza Horizon 4 2004 Vauxhall VX220
+addappid(1443061) -- Forza Horizon 4 1993 Hoonigan Ford Escort Cosworth Group A
+addappid(1443062) -- Forza Horizon 4 Lamborghini Gallardo LP570-4 Spyder Performante
+addappid(1443063) -- Forza Horizon 4 1959 Porsche 356A Coupe
+addappid(1443064) -- Forza Horizon 4 1963 Opel Kadett A
+addappid(1443065) -- Forza Horizon 4  1977 Hoonigan Ford Gymkhana 10 F-150
+addappid(1443066) -- Forza Horizon 4 2003 Honda S2000
+addappid(1443080) -- Forza Horizon 4 Treasure Map
+addappid(1443081) -- Forza Horizon 4 Formula Drift Car Pack
+addappid(1443082) -- Forza Horizon 4 Mitsubishi Car Pack
+addappid(1443083) -- Forza Horizon 4 Best of Bond Car Pack
+addappid(1443084) -- Forza Horizon 4 Barrett-Jackson Car Pack
+addappid(1443085) -- Forza Horizon 4 Icons Car Pack
+addappid(1443086) -- Forza Horizon 4 Performance Car Pack
+addappid(1443087) -- Forza Horizon 4 Any Terrain Car Pack
+addappid(1443088) -- Forza Horizon 4 British Sports Car Car Pack
+addappid(1443089) -- Forza Horizon 4 Welcome Pack
+addappid(1443090) -- Forza Horizon 4 VIP
+addappid(1443093) -- Forza Horizon 4 Car Pass
+addappid(1530010) -- Forza Horizon 4 Japanese Heroes Car Pack
+addappid(1530011) -- Forza Horizon 4 Open Top Car Pack
+addappid(1530012) -- Forza Horizon 4 Hot Wheels Legends Car Pack
