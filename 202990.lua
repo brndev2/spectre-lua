c2f5b76c3f5e3454ad43fda3da0cd671669d6411
Bruno <128260096+brndev2@users.noreply.github.com>
2026-07-17 02:55:09 -0300

    Add files via upload

---
+++ 202990.lua
@@ -0,0 +1,173 @@
+-- Generated with Luie @ https://lua.tools/
+-- 202990 - Call of Duty: Black Ops II - Multiplayer
+-- Generated 2026-07-17 03:56:00 UTC
+-- # Depots (Total/DLC/Shared): 67/6/19
+
+-- Main AppID
+addappid(202990, 1, "fde1d4d009be865cf9c14a571ca3d629a311267844e19864621bb0ca8a56170b")
+
+-- Main Depots
+addappid(202991, 1, "3724291585886fa5684621727fc589e13891b1ccdc96854826b8c317ee99cf34")
+setManifestid(202991, "4238126704581628502", 13259776)
+addappid(202992, 1, "e9a375aef2ac057d66568e4c2bb6940bc427729bd3f1c5ff151331e92cd2f6e1")
+setManifestid(202992, "6516382015411692772", 1141377728)
+addappid(203001, 1, "84d94e74ac1a14c6c71a702e3cef71b334d3e4b422a074262f6eb11385e5056f") -- English
+setManifestid(203001, "5470996728109450714", 501621234)
+addappid(203002, 1, "5b5d4db9e7fe28b9a30945b6340fddc2228ed77941e60367bdfc3c6ca5a316c4") -- French
+setManifestid(203002, "7941532629287986689", 495867955)
+addappid(203003, 1, "4cb9847cd10e3703eee53842fa7bd6d726ca36ff2c35a624e203e43d4563b744") -- Italian
+setManifestid(203003, "5212683482551898610", 505271233)
+addappid(203004, 1, "b7e78d53aa82a4744f0f471a29c4593dec22cfceb9a76e3174b3397e2528efe1") -- German
+setManifestid(203004, "5120191608293417335", 546977087)
+addappid(203005, 1, "5a647bc9a2fb140b679dc756812c661012954d3b11913816f73bf81f0f546a1d") -- Spanish
+setManifestid(203005, "1111866151159729246", 519651469)
+addappid(203007, 1, "ceb8a0c8273655d1f7c673f5947c1c9600748a45ac0c35544287c1e24f689f39") -- Polish
+setManifestid(203007, "5987465239495943787", 501657367)
+addappid(203008, 1, "a4c1161adceb805bb15750a42971c07b378acd7b1c6466369726200374f6c768") -- Russian
+setManifestid(203008, "6523841386678849139", 536161215)
+addappid(233171, 1, "a6bfcc58cb01418ca8863c989272bf466196f23f9a5c64fb16e7e32d58c29a4c") -- English
+setManifestid(233171, "3252521344750599734", 202176)
+addappid(233172, 1, "f1525c809799eccf2c3dfd6b16798089a0c38fdd049c8c2fbdfa38ad907d2281") -- French
+setManifestid(233172, "3479057479444067375", 200448)
+addappid(233173, 1, "acc0071107f9563dd4a031eb51a7c0858d0e4e0688c3e4350467c89af4451a1c") -- Italian
+setManifestid(233173, "4156915134672355984", 200576)
+addappid(233174, 1, "0f84ee8ebed71308b54c1881047e11e105e7c2254aa8a3223fc81cabc19ae652") -- German
+setManifestid(233174, "8584147097557246983", 200448)
+addappid(233175, 1, "031a8836c3c891ee5e5955f0675614bfcb6517f661892821622cee93f6ccd0eb") -- Spanish
+setManifestid(233175, "2629144150628209500", 200576)
+addappid(233176, 1, "e8325ccd46f52258cc63009ae442f20b5e51c4cd499cf05aff02108be7ceecef") -- Japanese
+setManifestid(233176, "4884962165551291910", 202176)
+addappid(233177, 1, "b0f0f6667676ec76c38a924785f0e918841ce5f427f1b2416e4234199e208b69") -- Polish
+setManifestid(233177, "5380343207170843421", 202176)
+addappid(233178, 1, "e5ba5070cc4ca5a56acd231d19943c2cec388ace8a0c988f5f2999b19bb5be9c") -- Russian
+setManifestid(233178, "3843783111114021449", 200576)
+addappid(238801, 1, "8fb3d75893951c2e08a8433044dc8d19c1e877b411d6d6b998cd1ab64fe93bcf") -- English
+setManifestid(238801, "5208633284234102366", 3328)
+addappid(238802, 1, "64632e11e487e37b22ca6c98c4f37b904e07361950af2bd749c5be890b8e3463") -- French
+setManifestid(238802, "1634473200831669456", 2048)
+addappid(238803, 1, "81346af220f909408cfa37700e0f6d3a07f62f60e67f5eee807800ba31e0842b") -- Italian
+setManifestid(238803, "5638320372907773330", 2048)
+addappid(238804, 1, "e51323d188a82db0da579671c0b5b7acfde9ce38f8a267639d1421a1d511668a") -- German
+setManifestid(238804, "4078122562082324285", 2048)
+addappid(238805, 1, "4977fbf5219bcb787678ecfaec9463c40ec0cfc641bdad67d1326e33f929aeea") -- Spanish
+setManifestid(238805, "3106058969024733278", 2048)
+addappid(238806, 1, "340ce47df0b9424b99d98394c067412394ba14fac313c7da5501db87176f405d") -- Japanese
+setManifestid(238806, "271408990813386872", 3328)
+addappid(238807, 1, "f5ad81efd0be3657b255d1127c8b7a866f13ee2ae0ca798505bd9ae644e3a594") -- Polish
+setManifestid(238807, "3844447364199732505", 3328)
+addappid(238808, 1, "3079dc029db127c099092f6507960052a9e147d8d7d44c21b707e561a4d049d9") -- Russian
+setManifestid(238808, "2853370377401460419", 2048)
+addappid(247161, 1, "91c81c73fcf13434a0d82437ddee9be08125486afcfd11aabf3a25b9603d1fad") -- English
+setManifestid(247161, "3827317568665009753", 397760)
+addappid(247162, 1, "77ee2d0bff2d51e155f1d68c2de5a8700c00563225ba3ff0d19835be63d95d23") -- French
+setManifestid(247162, "4239347740218693547", 395520)
+addappid(247163, 1, "c904035e7ab44627576143a0d1f1964713685485ddae361ab010d995cf122d31") -- Italian
+setManifestid(247163, "1423847016352745477", 395840)
+addappid(247164, 1, "e19a407ea2d30be3209b61a834bb3f1e7ff7cbc661ab584d2e70e7c337c2687b") -- German
+setManifestid(247164, "5114901825093107372", 395584)
+addappid(247165, 1, "d8992e19e6ecf6dc7a342a124b42b8cf60dfc2dfa2cbc556d2492d1f1f768982") -- Spanish
+setManifestid(247165, "5486576056577287460", 395840)
+addappid(247166, 1, "634281dd301ded428ca879944f80b1c5c0f4c78213b537797de45282f3d124f7") -- Japanese
+setManifestid(247166, "1968582005375953834", 397760)
+addappid(247167, 1, "db8b9879a348aef08c409414ba2d7ca80f0f5fe1e52ee2970d01de0e6f8f2fe2") -- Polish
+setManifestid(247167, "8287445821415075440", 397760)
+addappid(247168, 1, "1eba42b41dc18987005c4ab5cdd2716a6fe866a7ab94c48f45f1caf14147a501") -- Russian
+setManifestid(247168, "5905762178031471374", 395776)
+addappid(255561, 1, "a56da934a542729debd9504be49eb1fbb85ccdfb8376b3b30401c2043efc0f8a") -- English
+setManifestid(255561, "7076696022158943069", 202176)
+addappid(255562, 1, "6a706bbab5d89daf0b842a2c086d3d313d3289abd151fcc7ccfed0e8c13f328d") -- French
+setManifestid(255562, "2445978523351503570", 200448)
+addappid(255563, 1, "76300ffa34a9e5f268fdf88dc4a2a409fb5ffa704f5b804e7797277f0a96d856") -- Italian
+setManifestid(255563, "6480537727902873771", 200576)
+addappid(255564, 1, "60eb984698daf87558023203fc9796abba09e788ee040afda1c6f5f21123bcaf") -- German
+setManifestid(255564, "8542152767877907913", 200448)
+addappid(255565, 1, "d357b36bc0de588d315cf64bc3626b474a66ac114060b76ba040eba4328025fb") -- Spanish
+setManifestid(255565, "3247332054212560776", 200576)
+addappid(255566, 1, "f9421168e055e6b849fd9d92853ca1fca5abc99c0026ae5a93ab8efc7e121cf7") -- Japanese
+setManifestid(255566, "7728527539927494217", 202176)
+addappid(255567, 1, "bf47c57e53d0252271c9f31f077e037792a2accf76304761536e1b6d8e20a8df") -- Polish
+setManifestid(255567, "8576873804310123446", 202176)
+addappid(255568, 1, "96201ffebc19640d0e40045e22ea9d35d8fc3f71b5f5a50d3c726bc0fbe91756") -- Russian
+setManifestid(255568, "8460207384436530056", 200576)
+
+-- DLC's (no depot keys required)
+addappid(115307) -- DLC 115307
+addappid(115308) -- Call of Duty: Black Ops II - Russian DLC
+addappid(219092) -- Call of Duty: Black Ops II - Zombies - Nuketown Zombies
+addappid(234770) -- Call of Duty®: Black Ops II - Extra Slots Pack
+addappid(234771) -- Call of Duty®: Black Ops II - Party Rock MP Personalization Pack
+addappid(234772) -- Call of Duty®: Black Ops II - Kawaii MP Personalization Pack
+addappid(234773) -- Call of Duty®: Black Ops II - Graffiti MP Personalization Pack
+addappid(234774) -- Call of Duty®: Black Ops II Dia de los Muertos MP Personalization Pack
+addappid(234775) -- Call of Duty®: Black Ops II - Benjamins MP Personalization Pack
+addappid(234776) -- Call of Duty®: Black Ops II Bacon MP Personalization Pack
+addappid(234777) -- Call of Duty®: Black Ops II - Zombies MP Personalization Pack
+addappid(234778) -- Call of Duty®: Black Ops II - Viper MP Personalization Pack
+addappid(234779) -- Call of Duty®: Black Ops II - Jungle Warfare MP Personalization Pack
+addappid(234780) -- Call of Duty®: Black Ops II - North American Flags of the World Calling Card Pack
+addappid(234781) -- Call of Duty®: Black Ops II - South American Flags of the World Calling Card Pack
+addappid(234782) -- Call of Duty®: Black Ops II - European Flags of the World Calling Card Pack
+addappid(234783) -- Call of Duty®: Black Ops II - African Flags of the World Calling Card Pack
+addappid(234784) -- Call of Duty®: Black Ops II - Asian Flags of the World Calling Card Pack
+addappid(234785) -- Call of Duty: Black Ops II - Comics Pack
+addappid(234786) -- Call of Duty®: Black Ops II - Paladin Personalization Pack
+addappid(234787) -- Call of Duty: Black Ops II - Dragon Pack
+addappid(234788) -- Call of Duty®: Black Ops II - Cyborg Personalization Pack
+addappid(234789) -- Call of Duty: Black Ops II - Glam Pack
+addappid(234790) -- Call of Duty: Black Ops II - Coyote Pack
+addappid(234791) -- Call of Duty: Black Ops II - Pack-A-Punch Pack
+addappid(234792) -- Call of Duty: Black Ops II - Aqua Pack
+addappid(234793) -- Call of Duty: Black Ops II - Breach Pack
+addappid(234794) -- Call of Duty: Black Ops II - Rogue Pack
+addappid(274120) -- Call of Duty: Black Ops II - Dead Man’s Hand Pack
+addappid(274121) -- Call of Duty: Black Ops II - Beast Pack
+addappid(274122) -- Call of Duty: Black Ops II - Octane Pack
+addappid(274123) -- Call of Duty: Black Ops II - Weaponized 115 Pack
+addappid(292020) -- Call of Duty: Black Ops II - UK Punk Pack
+addappid(292021) -- Call of Duty: Black Ops II - Afterlife Pack
+
+-- DLC's (with depot keys)
+-- Call of Duty: Black Ops II - Nuketown 2025 (AppID: 219091)
+addappid(219091)
+addappid(219091, 1, "13db3f94308bbf15cedb716ddb4eacea88436d59a674ebb6faab079dc499baf0")
+setManifestid(219091, "3185257489268655163", 336598208)
+-- Call of Duty: Black Ops II - Soundtrack (AppID: 219094)
+addappid(219094)
+addappid(219094, 1, "2e5a53ff93c388da9e33c69ad6c761c937508465d431046c220ab3b564409ef3")
+-- Call of Duty®: Black Ops II Revolution (AppID: 219097)
+addappid(219097)
+addappid(219097, 1, "666df66958bcdb6a8362441d94cdb74319e93115e52c76f4e54b76b96b20b28c")
+setManifestid(219097, "8079780212554400363", 791801216)
+-- Call of Duty®: Black Ops II - Uprising (AppID: 219098)
+addappid(219098)
+addappid(219098, 1, "619907c2abcae91b02f6d777150635a00adcd1df47d3e072546502e8006229fa")
+setManifestid(219098, "1749099087036763551", 662244480)
+-- Call of Duty®: Black Ops II - Vengeance (AppID: 219101)
+addappid(219101)
+addappid(219101, 1, "fd41c1b9ddbbd9cc1a2109df66b0a585e09d923b2260845013295e0c77cd37b3")
+setManifestid(219101, "8727583105200031460", 746828352)
+-- Call of Duty: Black Ops II - Apocalypse (AppID: 219103)
+addappid(219103)
+addappid(219103, 1, "643975876cfc288fdee25bf481b45416f7a132ae9a9cacc736063da3137e1354")
+setManifestid(219103, "8054447117850293655", 786030272)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(202972, 1, "e8efd4d7d2c05ac03f3bd26bb3e9c8b0e83d58e3541f0e8f8da9f0a39f8fc16f")
+addappid(202973, 1, "f0de510f4bfceb5f47e865e94467b3f127d33c4d0d5c81914a3a29c4fc2c8590")
+addappid(202975, 1, "74ab26ff68cd38526c1f50213fe8f82a58ada56a0cb042c29823442525f0148a") -- English
+addappid(202976, 1, "725f44b140834a21ac2bbba5a199e498272a0bbe869a124ae99ef5b15c35cc12") -- French
+addappid(202977, 1, "3f8d2a2911e6b8edec42a23a388b45636588e7541d8abd02da2a62209b322be3") -- Italian
+addappid(202978, 1, "6c720b0ffae43e2e7584280239773ae1d31877c8b7ced5ba44b85d1cbe06c467") -- German
+addappid(202979, 1, "a111e2527dbecf81028bb41873e9b1ec64eea27a919a4e623ccd10340b6f6754") -- Spanish
+addappid(202996, 1, "a3efe5d0e7b9623c8732437c076280d2a6a8086bfaaf7e21833384a470ab7ff3") -- Polish
+addappid(202997, 1, "3688ca0dc337af67df29a0c8ee0b3702ca5db2d1ae69cc757344b10c7a76d135") -- Russian
+addappid(202998, 1, "4eae51754f9a91706005d9348c27f01e412062f6ff4db3a4dc7bf94529da60b6")
+addappid(212931, 1, "1bfa23fb44b6ae907d35c699df2ca329047a3fc8d799ac7adbe5f59b84a4a3fa") -- English
+addappid(212932, 1, "5582637895b55b41d978166c53e3056bcb2a47a423894b1677e66310986e645c") -- French
+addappid(212933, 1, "935387d11f582509a9c062fa90c44eea6e7e397ffaac3b7fd7768081426bae86") -- Italian
+addappid(212934, 1, "adfb1aaa13b4e1d0c5ec03003f643b4e73a0363d8599b415a3adce2ffd266fc5") -- German
+addappid(212935, 1, "9b4083ed36754d69bb425f84f7494eb880df3442907929dd4de84f9b84ca7462") -- Spanish
+addappid(212937, 1, "3cc03cb9f21b613b3be3d31083e4ff039a14699ed12aa19e7f357e43e12ba41c") -- Polish
+addappid(212938, 1, "fdda853da7f6551fe5981e5b5c468968cabe6d7236c78f2c5947017f4b86e9ff") -- Russian
+
+-- Missing Depots (We don't have the keys yet lol): 202995, 203006, 212936
