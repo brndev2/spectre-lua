fc3c906543d98cd71822922044ef211d1a8adcf3
Bruno <128260096+brndev2@users.noreply.github.com>
2026-07-21 10:20:32 -0300

    Add files via upload

---
+++ 2658510.lua
@@ -0,0 +1,11 @@
+-- Lua provided by SkyAPI 
+-- Game: Animal Shelter 2
+addappid(2658510)
+addappid(2658511, 1, "a48c2e31217f8135a3c94b510a5fcc9053ec566353f54bc6a17f9e42c2e36481")
+
+-- DLCS
+addappid(4831010)
+addappid(4831020)
+addappid(4613570)
+addappid(4247800)
+addappid(4119650)
