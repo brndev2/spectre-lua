ab6db3994b834b8abd777a019f8fccbc478d1436
Bruno <128260096+brndev2@users.noreply.github.com>
2026-07-24 18:33:36 -0300

    Add files via upload

---
+++ 2680010.lua
@@ -0,0 +1,8 @@
+-- Lua provided by SkyAPI 
+-- Game: The First Berserker: Khazan
+addappid(2680010)
+addappid(2680011, 1, "fe9a3ee6a263260b75cf97d27ffc4eeda8c9eebc8019bdc57c247a546f450077")
+
+-- DLC
+addappid(3265160)
+addappid(3265150)
---
+++ 2806050.lua
@@ -0,0 +1,7 @@
+-- Lua provided by SkyAPI 
+-- Game: Halo Campaign Evolved
+addappid(2806050, 1, "ed813e8e735c9a37bc3698804c2f49a73f4cd0ece4535e2b25d9f2f069abfe8e")
+addappid(2806051, 1, "400618d2330230a74b26331220f2aed43a92bf65eb23147779810f48f893c6e5") -- Depot 2806051
+addappid(4192200, 1, "ba2cc915e2f0112e4af89f7147a980880639c08aed6b47703282f5175355a021") -- Depot 4192200
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
