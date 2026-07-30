0d320e995cf6cc913eb0a5d84e288a874c9e9568
Bruno <128260096+brndev2@users.noreply.github.com>
2026-07-30 01:26:11 -0300

    Add files via upload

--- 1426210.lua
+++ 1426210.lua
@@ -1,4 +1,19 @@
--- Lua provided by SkyAPI 
--- Game: It Takes Two
-addappid(1426210)
+-- 1426210 - It Takes Two
+-- Generated 2026-07-30 04:09:12 UTC
+-- # Depots (Total/DLC/Shared): 5/2/2
+
+-- Main AppID
+addappid(1426210, 1, "ec7aca2b989b0dad7b2f7880268887d67d9656bfd0f47146e9cad7d411785250")
+
+-- Main Depots
 addappid(1426211, 1, "c450c44e032e27d0308cf864259fb2af91bcfc58401a0caf33bb8efba85bb323")
+setManifestid(1426211, "5290425664756095500", 48103037798)
+
+-- DLC's (no depot keys required)
+addappid(1426390) -- It Takes Two – Full Game Unlock
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 4110941, 4110942
