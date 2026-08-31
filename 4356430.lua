96a877f89c4e8521e1d455f85e623c319c11c5c0
Bruno <128260096+brndev2@users.noreply.github.com>
2026-08-31 19:24:43 -0300

    Add files via upload

---
+++ 4356430.lua
@@ -0,0 +1,25 @@
+-- Main AppID
+addappid(4356430)
+addtoken(4356430, "13716260120817756824")
+
+-- Main Depots
+addappid(4356431, 1, "a1ccdda87579dfa269b9015cc7ba18b7eb8339848b5eb408bfea8bdfb31eb8a4")
+setManifestid(4356431, "4442769665996928005", 80450406091)
+
+-- DLC's (no depot keys required)
+addappid(4357560) -- NBA 2K27 Pro Pass: Season 1
+addappid(4357570) -- NBA 2K27 Hall of Fame Pass: Season 1
+addappid(4357580) -- NBA 2K27 MyCAREER Bonus Offer: Season 1
+addappid(4357590) -- NBA 2K27 MyTEAM Bonus Offer: Season 1
+
+-- DLC's (with depot keys)
+-- NBA 2K27 - High Resolution Textures (AppID: 4687050)
+addappid(4687050)
+addtoken(4687050, "7614811867666966860")
+addappid(4687050, 1, "c96a0954f3836680afb31e3b837546bb92606ccaebcc3e1bf8cd3761b8221c25")
+setManifestid(4687050, "4767089068737929486", 29646686473)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 4841760, 4841761
