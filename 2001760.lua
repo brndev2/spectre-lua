ef2600be475f2803fd9553cf28a06f88b05e9e31
Bruno <128260096+brndev2@users.noreply.github.com>
2026-09-13 23:08:09 -0300

    Add files via upload

--- 2001760.lua
+++ 2001760.lua
@@ -1,18 +1,15 @@
--- 2001760 - Beast of Reincarnation
--- Generated 2026-08-07 05:04:53 UTC
--- # Depots (Total/DLC/Shared): 3/0/2
+-- Lua provided by RyzenAPI
+-- Game: Beast of Reincarnation
 
--- Main AppID
-addappid(2001760)
+-- MAIN APP DEPOTS / PACKAGES
+addappid(2001760, 1, "5a7d287208fe812255055e80d0e5f02b15836607d90c100f2760a85cbf70cb33") --Mainappid Beast of Reincarnation
+addappid(2001761, 1, "0d843c3fd4bba4400b829693c665e6394ad8311f3f7f6c0acf31c71eab887c0c") --Main Windows Depot Beast of Reincarnation
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") --Share Windows Depot Steamworks Common Redistributables
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") --Share Windows Depot Steamworks Common Redistributables
+addappid(4146810) --Dlcname Beast of Reincarnation Pre-Order DLC
+addappid(4146830) --Dlcname Beast of Reincarnation Deluxe DLC
 
--- Main Depots
-addappid(2001761, 1, "0d843c3fd4bba4400b829693c665e6394ad8311f3f7f6c0acf31c71eab887c0c")
-setManifestid(2001761, "5695605622489877808", 34142175971)
-
--- DLC's (no depot keys required)
-addappid(4146810) -- Beast of Reincarnation Pre-Order DLC
-addappid(4146830) -- Beast of Reincarnation Deluxe DLC
-
--- Shared Depots (Runtimes / Launchers / ETC)
-addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
-addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+-- MANIFESTS / UPDATES
+setManifestid(2001761, "5820605326682716309", 31561982976)
+setManifestid(228989, "5753583882400741046", 25108528)
+setManifestid(228990, "1829726630299308803", 100658080)
