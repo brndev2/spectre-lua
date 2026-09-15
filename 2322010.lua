ab414c011fbd855984316b75cf1fe546a56b7944
Bruno <128260096+brndev2@users.noreply.github.com>
2026-09-15 01:18:19 -0300

    Add files via upload

--- 2322010.lua
+++ 2322010.lua
@@ -1,6 +1,16 @@
--- Downloaded using DepotBox - https://depotbox.org/
--- Original file: 2322010.lua
+-- Lua provided by RyzenAPI
+-- Game: God of War Ragnarök
+
+-- MAIN APPLICATION
 addappid(2322010)
-setManifestid(228989,"3514306556860204959")
-addappid(2322011,0,"78758c85a2a8b04770fb4fe430ac7a215a959b72e496d94f6a0ec5ac38066f49")
-setManifestid(2322011,"3011634702942262327")
+addappid(2974310)
+addappid(2974320)
+addappid(2974330)
+
+-- MAIN APP DEPOTS / PACKAGES
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(2322011, 1, "78758c85a2a8b04770fb4fe430ac7a215a959b72e496d94f6a0ec5ac38066f49")
+
+-- MANIFESTS / UPDATES
+setManifestid(228989, "5753583882400741046")
+setManifestid(2322011, "151255971557114067")
