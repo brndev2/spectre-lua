8e07b7b4f6758c4c962e749319ecf602e8344596
Bruno <128260096+brndev2@users.noreply.github.com>
2026-09-17 01:00:43 -0300

    Add files via upload

--- 1888930.lua
+++ 1888930.lua
@@ -1,10 +1,16 @@
--- Downloaded using DepotBox - https://depotbox.org/
--- Original file: 1888930.lua
-addappid(1888930)
-setManifestid(228988,"6645201662696499616")
-addappid(1888931,0,"19cd197b4bc7e62551196ec84d00da135a4dedeb5edca50804001bf9e90b6b70")
-setManifestid(1888931,"4945188123625783686")
-addappid(1888933,0,"035887e659bc052ab1d1801d1665304be7da49992076cd123e25b9bd06a64134")
-setManifestid(1888933,"7178411350218387234")
-addappid(1888932,0,"20a675f9d2a0efd0cf0f8985b2a1c67d5d64e1c2b5aee665d3692969b28b1be4")
-setManifestid(1888932,"5557770133008488072")
+-- Game: The Last of Us™ Part I
+
+-- MAIN APP DEPOTS / PACKAGES
+addappid(1888930, 1, "7e63ee22bf6882ce64bbd414046ad3c0af40e3240a01daf293c7a47945c5c67f") --Mainappid The Last of Us™ Part I
+addappid(1888931, 1, "19cd197b4bc7e62551196ec84d00da135a4dedeb5edca50804001bf9e90b6b70") --Main Windows Depot The Last of Us™ Part I
+addappid(1888932, 1, "20a675f9d2a0efd0cf0f8985b2a1c67d5d64e1c2b5aee665d3692969b28b1be4") --Main Windows Depot The Last of Us™ Part I
+addappid(1888933, 1, "035887e659bc052ab1d1801d1665304be7da49992076cd123e25b9bd06a64134") --Main Windows Depot The Last of Us™ Part I
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") --Share Windows Depot Steamworks Common Redistributables
+addappid(2245260) --Dlcname The Last of Us™ Part I - Pre-purchase Entitlements
+addappid(2254450) --Dlcname The Last of Us™ Part I - Upgrade to Digital Deluxe Edition
+
+-- MANIFESTS / UPDATES
+setManifestid(1888931, "4945188123625783686", 67220167952)
+setManifestid(1888932, "5557770133008488072", 14361811264)
+setManifestid(1888933, "7178411350218387234", 3001829600)
+setManifestid(228988, "6645201662696499616", 22411856)
