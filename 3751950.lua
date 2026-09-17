aecd31dfa08e1d0580f4fe8837c01493630bcf0f
Bruno <128260096+brndev2@users.noreply.github.com>
2026-09-16 23:22:34 -0300

    Add files via upload

--- 3751950.lua
+++ 3751950.lua
@@ -1,30 +1,7 @@
--- 3751950 - Assassin's Creed Black Flag Resynced
--- Version-locked to Build 24424450 — released 2026-08-04 13:58:25 UTC
--- Generated 2026-08-07 05:26:55 UTC
--- # Depots (Total/DLC/Shared): 10/0/2
+-- Lua provided by RyzenAPI
+-- Game: Assassin's Creed Black Flag Resynced
 
--- Main AppID
-addappid(3751950, 1, "e6d96386c77349411f5be39f2955ee0649a9a80abd22c3d69752bfc6e3302539")
-
--- Main Depots
-addappid(3751951, 1, "0495628add2f29892c7a7930e1ed5332f68e374d7621e3338f23492fcd8f23db")
-setManifestid(3751951, "4397710407098141927")
-addappid(3751953, 1, "f96dc68fd865006d30d7d804bab42593f8bb6a5250624df45771cbc2a9c7aa45") -- French
-setManifestid(3751953, "3022415893432196011")
-addappid(3751954, 1, "b61e5b022604f5f47312b82b8489487c5b1dc57159c8ee09ecac0c7959631762") -- Italian
-setManifestid(3751954, "6892818699399123643")
-addappid(3751955, 1, "f2ae043d5d7111cfd549ed1bd3f0611cd26711aa4a55e1af56889e43e8d0fb1c") -- German
-setManifestid(3751955, "4309823939662694323")
-addappid(3751956, 1, "c6c9dba85bf9b4a6f19551cc87b047ab631cd88317027eb33e30174e4bbe41fc") -- Spanish
-setManifestid(3751956, "6870137386025150758")
-addappid(3751957, 1, "c98ba5f83bc7269be83bedef8689d8a68ff2bb4ad1d6f7ba221178305af557c4") -- Brazilian
-setManifestid(3751957, "4086000294114352953")
-addappid(3751958, 1, "17b0d6cff686c37972334878f48648f8982f40f290fdaeddc022e1a795e6452c") -- Japanese
-setManifestid(3751958, "5424793249443767029")
-addappid(3751959, 1, "6011c824af6fe4db62d3154d751de76d49422e4a56218d54d8ceb716cac7fa6c") -- Schinese
-setManifestid(3751959, "6456623308878089663")
-
--- DLC's (no depot keys required)
+-- MAIN APP DEPOTS / PACKAGES
 addappid(4496490) -- Assassin's Creed Black Flag Resynced - Master Assassin Character Pack
 addappid(4496500) -- Assassin's Creed Black Flag Resynced - Master Assassin Character Pack - Ubisoft Activation
 addappid(4496510) -- Assassin's Creed Black Flag Resynced - Master Assassin Naval Pack
@@ -49,7 +26,26 @@ addappid(4519940) -- Assassin's Creed Black Flag Resynced - Standard Edition - P
 addappid(4519950) -- Assassin's Creed Black Flag Resynced - Deluxe Edition - Prepurchase - Ubisoft Activation
 addappid(4872930) -- Assassin's Creed Black Flag Resynced - Standard Edition - PREVIEW - Ubisoft Activation
 addappid(4892480) -- Assassin's Creed Black Flag Resynced - Deluxe Edition - PREVIEW - Ubisoft Activation
-
--- Shared Depots (Runtimes / Launchers / ETC)
 addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
 addappid(1716751, 1, "84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675")
+addappid(3751950, 1, "e6d96386c77349411f5be39f2955ee0649a9a80abd22c3d69752bfc6e3302539")
+addappid(3751951, 1, "0495628add2f29892c7a7930e1ed5332f68e374d7621e3338f23492fcd8f23db")
+addappid(3751953, 1, "f96dc68fd865006d30d7d804bab42593f8bb6a5250624df45771cbc2a9c7aa45") -- French
+addappid(3751954, 1, "b61e5b022604f5f47312b82b8489487c5b1dc57159c8ee09ecac0c7959631762") -- Italian
+addappid(3751955, 1, "f2ae043d5d7111cfd549ed1bd3f0611cd26711aa4a55e1af56889e43e8d0fb1c") -- German
+addappid(3751956, 1, "c6c9dba85bf9b4a6f19551cc87b047ab631cd88317027eb33e30174e4bbe41fc") -- Spanish
+addappid(3751957, 1, "c98ba5f83bc7269be83bedef8689d8a68ff2bb4ad1d6f7ba221178305af557c4") -- Brazilian
+addappid(3751958, 1, "17b0d6cff686c37972334878f48648f8982f40f290fdaeddc022e1a795e6452c") -- Japanese
+addappid(3751959, 1, "6011c824af6fe4db62d3154d751de76d49422e4a56218d54d8ceb716cac7fa6c") -- Schinese
+
+-- MANIFESTS / UPDATES
+setManifestid(3751951, "4397710407098141927")
+setManifestid(3751953, "3022415893432196011")
+setManifestid(3751954, "6892818699399123643")
+setManifestid(3751955, "4309823939662694323")
+setManifestid(3751956, "6870137386025150758")
+setManifestid(3751957, "4086000294114352953")
+setManifestid(3751958, "5424793249443767029")
+setManifestid(3751959, "6456623308878089663")
+setManifestid(228989, "5753583882400741046")
+setManifestid(1716751, "6659642105086821873")
